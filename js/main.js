760ec026dc9ea5a0f0977e9af0825ff9ed838a43
nomandhoni-cs <nomandhoni@yahoo.com>
2022-04-14 19:57:25 +0600

    Buttons are working and click to subtotal price working

--- js/main.js
+++ js/main.js
@@ -8,20 +8,49 @@ function increaseDecreaseProduct(valueYouWantToChange, incrementAmount) {
 
 //Price Changer Function
 
-function increaseDecreaseHandler(valueYouWantToChange, incrementAmount) {
+function increaseDecreasePrice(valueYouWantToChange, incrementAmount) {
   const value = document.getElementById(valueYouWantToChange).innerText;
   const integerValue = parseInt(value);
   const updatedValue = integerValue + incrementAmount;
   document.getElementById(valueYouWantToChange).innerText = updatedValue;
 }
 
+//Make Text into float
+function textToFloat(textId) {
+  var textPrice = document.getElementById(textId).innerText;
+  const floatedPrice = parseFloat(textPrice);
+  return floatedPrice;
+}
+
+const iphonePriceFloat = textToFloat("iphonePrice");
+const casePriceFloat = textToFloat("casePrice");
+
+//Device Amount Decrease
 const decreaseDevice = document.getElementById("decreaseDevice");
 decreaseDevice.addEventListener("click", function () {
   increaseDecreaseProduct("deviceAmount", -1);
+  increaseDecreasePrice("subTotalPrice", -1 * iphonePriceFloat);
 });
+
+//Device Amount Increase
 const increaseDevice = document.getElementById("increaseDevice");
 increaseDevice.addEventListener("click", function () {
   increaseDecreaseProduct("deviceAmount", 1);
+  increaseDecreasePrice("subTotalPrice", iphonePriceFloat);
+});
+
+//Case Amount Decrease
+const decreaseCase = document.getElementById("decreaseCase");
+decreaseCase.addEventListener("click", function () {
+  increaseDecreaseProduct("caseAmount", -1);
+  increaseDecreasePrice("subTotalPrice", -1 * casePriceFloat);
+});
+
+//Case Amount Increase
+const increaseCase = document.getElementById("increaseCase");
+increaseCase.addEventListener("click", function () {
+  increaseDecreaseProduct("caseAmount", 1);
+  increaseDecreasePrice("subTotalPrice", casePriceFloat);
 });
 //Above incrementAmount will be in number such as -1 = decrease 1
 // and 1 = increase value by 1
